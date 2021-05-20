b1901bba4ff2c10cff715cf58c90883896acc531
juanmari <jcelihueta@gipuzkoa.eus>
2021-05-20 11:28:14 +0200

    Calculate distances between latitude-longitude coordinates

--- profile.php
+++ profile.php
@@ -8,14 +8,35 @@ $coors = $_REQUEST["coors"];
 $srs = $_REQUEST["srs"];
 $fmt = $_REQUEST["format"];
 
-// Function to get altitude value
-function altitude($x1, $y1, $srs, $session_id) {
+// Function to get altitude values
+function altitude($x1, $y1, $x2, $y2, $srs, $session_id) {
 	$wcs = "https://b5mprod/ogc/wcs/gipuzkoa_wcs?SERVICE=WCS&VERSION=1.0.0&REQUEST=GetCoverage&FORMAT=GEOTIFFFLOAT32&COVERAGE=LIDAR%202008:%20Lur%20eredua-Modelo%20de%20suelo&CRS=" . $srs . "&RESPONSE_CRS=" . $srs . "&WIDTH=10&HEIGHT=10&BBOX=";
 	if (strtolower($srs) == "epsg:4326") {
 		$offset = 0.0001;
 	} else {
 		$offset = 1;
 	}
+
+	// Distance of the line
+	if (strtolower($srs) == "epsg:4326") {
+		// Distance from latitude and longitude
+    // Converting to radians
+    $longi1 = deg2rad($x1);
+    $longi2 = deg2rad($x2);
+    $lati1 = deg2rad($y1);
+    $lati2 = deg2rad($y2);
+
+    // Haversine Formula
+    $difflong = $longi2 - $longi1;
+    $difflat = $lati2 - $lati1;
+
+    $val = pow(sin($difflat / 2), 2) + cos($lati1) * cos($lati2) * pow(sin($difflong / 2), 2);
+    $distance =6378800 * (2 * asin(sqrt($val)));
+	} else {
+		$distance = sqrt(pow($x2 - $x1, 2)+pow($y2 - $y1, 2));
+	}
+	return "$x1 $y1 $x2 $y2 $distance";
+
 	$x2 = $x1 + $offset;
 	$y2 = $y1 - $offset;
 	$wcs_query = $wcs . $x1 . "," . $y1 . "," . $x2 . "," . $y2;
@@ -56,12 +77,24 @@ $coors_array = explode(",", $coors);
 $i = 1;
 foreach ($coors_array as $coor)  {
 	if ($i == 1) {
-		$coor1 = $coor;
-	} else {
-		$coor2 = $coor;
-		$altitude = altitude($coor1, $coor2, $srs, $session_id);
-		$i = 0;
-		echo $coor1 . " - " . $coor2 . " - " . $altitude ."<br />";
+		$x1 = $coor;
+	} else if ($i == 2) {
+		$y1 = $coor;
+	} else if ($i == 3) {
+		$x2 = $coor;
+	} else if ($i == 4) {
+		$y2 = $coor;
+		echo "KK1: $x1 $y1 $x2 $y2 <br />";
+		$profile = altitude($x1, $y1, $x2, $y2, $srs, $session_id);
+		echo "KK2: $profile <br />";
+		$x1 = $x2;
+		$y1 = $y2;
+		$i = 2;
+//	} else {
+//		$coor2 = $coor;
+//		$altitude = altitude($coor1, $coor2, $srs, $session_id);
+//		$i = 0;
+//		echo $coor1 . " - " . $coor2 . " - " . $altitude ."<br />";
 	}
 	$i++;
 }
