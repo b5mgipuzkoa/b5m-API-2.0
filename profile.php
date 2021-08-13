52675583dbccf2cfe19e3376ed43f35e425be0b3
juanmari <jcelihueta@gipuzkoa.eus>
2021-08-13 14:48:52 +0200

    API to make topographic profiles improved (speed and accuracy)

--- profile.php
+++ profile.php
@@ -3,205 +3,104 @@
 //
 // Data comes from the b5m OGC WCS Service
 //
-// Coordinate transformation with PROJ (proj.org): cs2cs
-// Raster query to get heights with GDAL (gdal.org): gdallocationinfo
+// Requires profile.sh Bash script to calculate the profile
+//
+// Coordinate transformation with PROJ (proj.org)
+// Raster query to get heights with GDAL (gdal.org)
 //
 
 // Requests
-$coors = $_REQUEST["coors"];
-$srs = $_REQUEST["srs"];
-$format = $_REQUEST["format"];
-$show_coors = $_REQUEST["show_coors"];
-if (empty($show_coors)) $show_coors = 0;
+if (isset($_REQUEST['coors'])) $coors = $_REQUEST['coors']; else $coors = "";
+if (isset($_REQUEST['srs'])) $srs = $_REQUEST['srs']; else $srs = "";
+if (isset($_REQUEST['precision'])) $precision = $_REQUEST['precision']; else $precision = "";
+if (isset($_REQUEST['format'])) $format = $_REQUEST['format']; else $format = "";
 
 // Includes
 include_once("includes/json2xml.php");
 
 // Variables
-$wcs = "https://b5mprod/ogc/wcs/gipuzkoa_wcs?SERVICE=WCS&VERSION=1.0.0&REQUEST=GetCoverage&FORMAT=GEOTIFFFLOAT32&COVERAGE=LIDAR%202008:%20Lur%20eredua-Modelo%20de%20suelo&CRS=";
-$srs_global = "epsg:25830";
-$offset = 0.1;
-$width_wcs = 1;
-$height_wcs = $width_wcs;
-$distance_min = 20; // Minimum distance between two points
-$point_max = 40; // Maximum number of points in a section
-$statuscode = 1;
+$server_name = $_SERVER['SERVER_NAME'];
+$max_number_coors = 20;
+
+// Messages
+$msg001="Warning: there are cases out of our range (Gipuzkoa: https://b5m.gipuzkoa.eus) where no height data exists, and the resulting height value is -9999";
+$msg002="Missing required parameter: coors";
+$msg003="SRS not supported. It should be: EPSG:25830, EPSG:4326 or EPSG:3857 (https://epsg.io)";
+$msg004="The maximum number of coordinate pairs has been exceeded (" . $max_number_coors . ")";
 
 // License and metadata variables
 $provider = "b5m - Gipuzkoa Spatial Data Infrastructure - Gipuzkoa Provincial Council - 2021";
-$url_license = "https://b5m.gipuzkoa.eus/web5000/en/legal-information";
-$url_base = "https://b5m.gipuzkoa.eus";
-$image_url = "https://b5m.gipuzkoa.eus/api/2.0/info/b5m_logo.png";
+$url_license = "https://" . $server_name . "/web5000/en/legal-information";
+$url_base = "https://" . $server_name . "/web5000";
+$image_url = "https://" . $server_name . "/web5000/assets/img/logo-b5m.svg";
 $altimetry_data = "Digital Terrain Model (DTM) of 1m from the Autonomous Community of the Basque Country, based on LIDAR data. Year 2012";
 $altimetry_data_url = "http://www.geo.euskadi.eus/lidar-lur-zoruaren-eredu-digitalean-oinarritutako-euskal-autonomi-erkidegoko-norantza-mapa-2012-urtea/s69-geodir/eu";
 $altimetry_units = "meters";
 $response_time_units = "seconds";
 $messages = "";
 
-// Function to convert coordinates
-function coordinate_convert($x, $y, $srs1, $srs2)
-{
-	global $floating_number;
-	$conversion_array1 = explode(" ", exec("echo \"" . $x . " " . $y . "\" | cs2cs -f \"%." . $floating_number . "f\" +init=" . $srs1 . " +to +init=" . $srs2));
-	$conversion_array2 = explode("	", $conversion_array1[0]);
-	return $conversion_array2[0] . " " . $conversion_array2[1];
-}
-
-// Function to get height values
-function height($x, $y) {
-	global $session_id, $srs_global, $offset, $width_wcs, $height_wcs, $wcs;
-	$wcs2 = $wcs . $srs_global . "&RESPONSE_CRS=" . $srs_global . "&WIDTH=" . $width_wcs . "&HEIGHT=" . $height_wcs . "&BBOX=";
-
-	$xo = $x + $offset;
-	$yo = $y - $offset;
-	$wcs_query = $wcs2 . $x . "," . $y . "," . $xo . "," . $yo;
-	$f_name = "/tmp/" . $session_id . ".tif";
-
-	$ch = curl_init($wcs_query);
-	$fp = fopen($f_name, "w");
-
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_FILE, $fp);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-
-	curl_exec($ch);
-	if(curl_error($ch)) {
-	    fwrite($fp, curl_error($ch));
-	}
-	curl_close($ch);
-	fclose($fp);
-
-	$output = null;
-	$retval = null;
-	exec("gdallocationinfo " . $f_name . " 0 0", $output, $retval);
-
-	unlink($f_name);
-
-	$height = explode(" ", $output[3]);
-	return $height[5];
-}
-
-// Profile points making function
-function profile_points($x1_srs_ori, $y1_srs_ori, $x2_srs_ori, $y2_srs_ori, $srs) {
-	global $srs_global, $distance_min, $point_max, $show_coors;
-
-	// Converting to $srs_global to calculate the distance between two points
-	if (strtolower($srs) != $srs_global) {
-		$coord1_array = explode(" ", coordinate_convert($x1_srs_ori, $y1_srs_ori, strtolower($srs), $srs_global));
-		$coord2_array = explode(" ", coordinate_convert($x2_srs_ori, $y2_srs_ori, strtolower($srs), $srs_global));
-		$x1 = $coord1_array[0];
-		$y1 = $coord1_array[1];
-		$x2 = $coord2_array[0];
-		$y2 = $coord2_array[1];
-	}	else {
-		$x1 = $x1_srs_ori;
-		$y1 = $y1_srs_ori;
-		$x2 = $x2_srs_ori;
-		$y2 = $y2_srs_ori;
-	}
-
-	// Distance of the line
-	$distance = sqrt(pow($x2 - $x1, 2) + pow($y2 - $y1, 2));
-
-	// Coordinates to query
-	if ($distance <= $distance_min) {
-		$coor_query_array[0] = $x1 . " " . $y1 . " 0.00";
-		$coor_query_array[1] = $x2 . " " . $y2 . " " . $distance;
-	} else {
-		$number_of_points = floor($distance / $distance_min);
-		if ($number_of_points > $point_max) {
-			$distance_min = $distance / $point_max;
-			$number_of_points = $point_max - 1;
-		}
-		$coor_query_array[0] = $x1 . " " . $y1 . " " . $x1_srs_ori . " " . $y1_srs_ori . " 0.00";
-
-		// Calculation of intermediate points
-		$x_offset = (($x2 - $x1) / $distance) * $distance_min;
-		$y_offset = (($y2 - $y1) / $distance) * $distance_min;
-		$x_inter_new = $x1;
-		$y_inter_new = $y1;
-		for ($k = 1; $k <= $number_of_points; $k++) {
-			$x_inter = $x_inter_new + $x_offset;
-			$y_inter = $y_inter_new + $y_offset;
-			$x_inter_new = $x_inter;
-			$y_inter_new = $y_inter;
-
-			// Intermediate point in the original srs
-			if ((strtolower($srs) != $srs_global) && ($show_coors == 1)) {
-				$coord3_array = explode(" ", coordinate_convert($x_inter, $y_inter, $srs_global, strtolower($srs)));
-				$x_inter_srs_ori = $coord3_array[0];
-				$y_inter_srs_ori = $coord3_array[1];
-			} else {
-				$x_inter_srs_ori = $x_inter;
-				$y_inter_srs_ori = $y_inter;
-			}
-			$coor_query_array[$k] = $x_inter . " " . $y_inter . " " . $x_inter_srs_ori . " " . $y_inter_srs_ori . " " . $distance_min;
-		}
-		$distance_final = sqrt(pow($x2 - $x_inter, 2) + pow($y2 - $y_inter, 2));
-		$coor_query_array[$k] = $x2 . " " . $y2 . " " . $x2_srs_ori . " " . $y2_srs_ori . " " . $distance_final;
-	}
-	return $coor_query_array;
-}
-
-// Session start
-$session_id = session_create_id();
-session_id($session_id);
-session_start();
-
-// Floating number of coordinates
-if (strtolower($srs) == "epsg:4326") {
-	$floating_number = 6;
-} else {
-	$floating_number = 2;
-}
-
-// Show entered coordinates and origin SRS
-$doc2["coordinates"] = $coors;
-$doc2["srs"] = $srs;
-
-// Coordinates loop
+// Init statuscode and time
+$statuscode = 0;
 $init_time = microtime(true);
-$distance_profile = 0;
-$coors_array = explode(",", $coors);
-$i = 1;
-$j = 1;
-foreach ($coors_array as $coor)  {
-	$statuscode = 0;
-	if ($i == 1) {
-		$x1 = $coor;
-	} else if ($i == 2) {
-		$y1 = $coor;
-	} else if ($i == 3) {
-		$x2 = $coor;
-	} else if ($i == 4) {
-		$y2 = $coor;
-		$profile_array = profile_points($x1, $y1, $x2, $y2, $srs);
 
-		// Profile loop
-		foreach ($profile_array as $profile_coor)  {
-			$profile_array_2 = explode(" ", $profile_coor);
-			$x_fin = $profile_array_2[0];
-			$y_fin = $profile_array_2[1];
-			$x_fin_srs_ori = $profile_array_2[2];
-			$y_fin_srs_ori = $profile_array_2[3];
-			$distance_fin = $profile_array_2[4];
-			if (($j == 1) || ($distance_fin != 0)) {
-				$distance_profile = $distance_profile + $distance_fin;
-				$height = height($x_fin, $y_fin);
-				if ($show_coors == 1) {
-						// Show point coordinates
-						$doc2["elevationProfile"][$j-1]["coordinates"] = sprintf("%." . $floating_number . "f", $x_fin_srs_ori) . "," . sprintf("%." . $floating_number . "f", $y_fin_srs_ori);
-				}
-				$doc2["elevationProfile"][$j-1]["distance"] = sprintf("%.2f", $distance_profile);
-				$doc2["elevationProfile"][$j-1]["height"] = sprintf("%.2f", $height);
-				$j++;
-			}
-		}
-		$x1 = $x2;
-		$y1 = $y2;
-		$i = 2;
+// Precision
+if (($precision > 1) && ($precision < 6)){
+	$precision = $precision;
+} else {
+	$precision = 1;
+}
+
+// Coordinates
+if ($coors == "") {
+	$statuscode = 2;
+	$messages = $msg002;
+}
+
+// SRS
+if (($srs != "") && (strtolower($srs) != "epsg:25830") && (strtolower($srs) != "epsg:4326") && (strtolower($srs) != "epsg:3857")) {
+	$statuscode = 3;
+	$messages = $msg003;
+}
+
+// Maximum number of coordinates
+$coors_array = explode(",", $coors);
+if (count($coors_array) / 2 > $max_number_coors) {
+	$statuscode = 4;
+	$messages = $msg004;
+}
+
+// Trying to guess the SRS
+if ($srs == "") {
+	if (($coors_array[0] >= -90) && ($coors_array[0] <= 90)) {
+		$srs = "epsg:4326";
+	} else if ($coors_array[0] > 90) {
+		$srs = "epsg:25830";
+	} else {
+		$srs = "epsg:3857";
 	}
-	$i++;
+}
+
+if ($statuscode == 0) {
+	// Data request
+	$output = shell_exec("./profile.sh $coors $srs $precision");
+	$data = explode("\n", $output);
+	$data_last = array_pop($data);
+
+	$i = 0;
+	foreach ($data as $value) {
+		$value_array = explode(" ", $value);
+		$doc2["elevationProfile"][$i]["distance"] = $value_array[0];
+		$doc2["elevationProfile"][$i]["height"] = $value_array[1];
+
+		// Detecting out of range values
+		if ($value_array[1] == "-9999") {
+			$statuscode = 1;
+			$messages = $msg001;
+		}
+		$i++;
+	}
+} else {
+	$doc2 = [];
 }
 
 // Data license and metadata
@@ -218,6 +117,9 @@ $doc1["info"]["responseTime"]["time"] = $response_time;
 $doc1["info"]["responseTime"]["units"] = $response_time_units;
 $doc1["info"]["statuscode"] = $statuscode;
 $doc1["info"]["messages"] = $messages;
+$doc1["coordinates"] = $coors;
+$doc1["srs"] = $srs;
+$doc1["precision"] = $precision;
 
 // Merge Arrays
 $doc = array_merge($doc1, $doc2);
@@ -236,7 +138,3 @@ if ((strtolower($format) == "php") || (strtolower($format) == "phps")) {
 		print_r($jsonres);
 	}
 }
-
-// Session end
-session_destroy();
-session_write_close();
---
+++ profile.sh
@@ -0,0 +1,218 @@
+#!/bin/bash
+#
+# Topographic profile from coordinates and a LIDAR file with the height data
+#
+# Taken from Koko Alberti's idea:
+# https://kokoalberti.com/articles/creating-elevation-profiles-with-gdal-and-two-point-equidistant-projection/
+#
+# Requeriments: GDAL (https://gdal.org) and PROJ4 (http://proj4js.org)
+
+if [ $# -ne 1 ] && [ $# -ne 2 ] && [ $# -ne 3 ]
+then
+	echo "Usage: $0 coors epsg precision [$0 592000,4800000,592090,4799910 EPSG:25830 precision]"
+	exit 1
+fi
+
+# precision
+if [ "$3" = "" ]
+then
+	precision=1
+else
+	precision=$3
+fi
+
+# Entry variables
+coors="$1"
+srs="$(echo "$2" | gawk '{print tolower($1)}')"
+
+# Variables
+let length_choice=3000/$precision
+srs_base="epsg:25830"
+base="/tmp/$$"
+d_lidar="/home9/lidar"
+f_lidar1="${d_lidar}/suelo2008_2.tif"
+f_lidar2="${d_lidar}/suelo2008_05.tif"
+f_lidar3="${d_lidar}/suelo2008_10.tif"
+f_lidar4="${d_lidar}/suelo2008_20.tif"
+f_lidar5="${d_lidar}/suelo2008_40.tif"
+f_lidar6="${d_lidar}/suelo2008_80.tif"
+f1="${base}.csv"
+f2="${base}.gpkg"
+f3="${base}.tif"
+f4="${base}.xyz"
+f5="${base}.txt"
+
+profile_2p () {
+	# Calculation of the profile from two points
+
+	# If not SRS, trying to regcognize it
+	if [ "$srs" = "" ]
+	then
+		if ((($(echo "$x1 <= 90" | bc -l))&&($(echo "$x1 >= -90" | bc -l))))
+		then
+			srs="epsg:4326"
+		elif (($(echo "$x1 > 90" | bc -l)))
+		then
+			srs="epsg:25830"
+		else
+			srs="epsg:3987"
+		fi
+	fi
+
+	# Coordinate conversion
+	if [ "$srs" = "epsg:3857" ]
+	then
+		coor_conv1="$(echo "$x1 $y1" | cs2cs -f "%.4f" +init="$srs" +to +init="$srs_base")"
+		coor_conv2="$(echo "$x2 $y2" | cs2cs -f "%.4f" +init="$srs" +to +init="$srs_base")"
+		x1c="$(echo "$coor_conv1" | gawk '{print $1}')"
+		y1c="$(echo "$coor_conv1" | gawk '{print $2}')"
+		x2c="$(echo "$coor_conv2" | gawk '{print $1}')"
+		y2c="$(echo "$coor_conv2" | gawk '{print $2}')"
+	else
+		x1c="$x1"
+		y1c="$y1"
+		x2c="$x2"
+		y2c="$y2"
+	fi
+
+	# Length of the line
+	rm "$f1" 2> /dev/null
+	echo "$x1c $y1c $x2c $y2c" | gawk '
+	BEGIN {
+		i=1
+		print "\"ID\", \"WKT\""
+	}
+	{
+		printf("\"%s\", \"LINESTRING(%s %s, %s %s)\"\n",i,$1,$2,$3,$4)
+		i++
+	}
+	' > "$f1"
+	rm "$f2" 2> /dev/null
+	ogr2ogr -f "GPKG" -s_srs "$srs" -t_srs "$srs" -nln "line" "$f2" "$f1" 2> /dev/null
+	if [ "$srs" = "epsg:4326" ]
+	then
+		use_ellipsoid=",true"
+	else
+		use_ellipsoid=""
+	fi
+	rm "$f1" 2> /dev/null
+	l1="$(ogrinfo "$f2" -q -dialect sqlite -sql "select st_length(geom${use_ellipsoid}) from line" 2> /dev/null | gawk '
+	{
+		if (($1=="st_length(geom)")||($1=="st_length(geom,true)"))
+			print $NF
+	}
+	')"
+	rm "$f2" 2> /dev/null
+
+	# LIDAR file choice depending on the length of the line
+	if (($(echo "$l1 <= $length_choice" | bc -l)))
+	then
+		f_lidar="$f_lidar1"
+	elif (($(echo "$l1 <= $length_choice * 2" | bc -l)))
+	then
+		f_lidar="$f_lidar2"
+	elif (($(echo "$l1 <= $length_choice * 4" | bc -l)))
+	then
+		f_lidar="$f_lidar3"
+	elif (($(echo "$l1 <= $length_choice * 8" | bc -l)))
+	then
+		f_lidar="$f_lidar4"
+	elif (($(echo "$l1 <= $length_choice * 16" | bc -l)))
+	then
+		f_lidar="$f_lidar5"
+	else
+		f_lidar="$f_lidar6"
+	fi
+
+	# LIDAR file resolution
+	res1="$(gdalinfo "$f_lidar" | gawk '{if($1=="Pixel"){split($4,a,"(");split(a[2],b,",");printf("%.0f\n",b[1])}}')"
+
+	# Warping data into a swath with two-point equidistant projection
+	l2="$(echo "$l1 $res1" | gawk '{printf("%.0f\n",$1/$2)}')"
+	l3=1
+	xl2="$(echo "$l1" | gawk '{printf("%f\n",$1/2)}')"
+	xl1="-${xl2}"
+	yl2="$(echo "$res1" | gawk '{printf("%f\n",$1/2)}')"
+	yl1="-${yl2}"
+	if [ "$srs" != "epsg:4326" ]
+	then
+		coor1="$(echo "$x1 $y1" | cs2cs -f "%.12f" +init="$srs" +to +init=epsg:4326)"
+		coor2="$(echo "$x2 $y2" | cs2cs -f "%.12f" +init="$srs" +to +init=epsg:4326)"
+		x3="$(echo "$coor1" | gawk '{print $1}')"
+		y3="$(echo "$coor1" | gawk '{print $2}')"
+		x4="$(echo "$coor2" | gawk '{print $1}')"
+		y4="$(echo "$coor2" | gawk '{print $2}')"
+	else
+		x3="$x1c"
+		y3="$y1c"
+		x4="$x2c"
+		y4="$y2c"
+	fi
+	proj_tpeqd="+proj=tpeqd +lon_1=${x3} +lat_1=${y3} +lon_2=${x4} +lat_2=${y4}"
+	rm "$f3" 2> /dev/null
+	gdalwarp -overwrite -t_srs "$proj_tpeqd" -te $xl1, $yl1, $xl2, $yl2 -ts $l2 $l3 -r near "$f_lidar" "$f3" > /dev/null 2> /dev/null
+
+	# Obtaining height data
+	rm "$f4" 2> /dev/null
+	gdal_translate -of "XYZ" "$f3" "$f4" > /dev/null 2> /dev/null
+	res2="$(gdalinfo "$f3" | gawk '{if($1=="Pixel"){split($4,a,"(");split(a[2],b,",");printf("%f\n",b[1])}}')"
+	rm "$f3" 2> /dev/null
+
+	# Height data processing
+	gawk -v res2=$res2 '
+	{
+		printf("%s,%.2f\n",res2,$3)
+	}
+	' "$f4"
+	rm "$f4" 2> /dev/null
+}
+
+# Coordinate reading
+rm "$f5" 2> /dev/null
+i=1
+j=0
+IFS=',' read -a coors_array <<< "$coors"
+for coor in "${coors_array[@]}"
+do
+	case "${i}" in
+	  1) x1="$coor";;
+	  2) y1="$coor";;
+	  3) x2="$coor";;
+	  4) y2="$coor";;
+	esac
+	let i=$i+1
+
+	if [ $i -eq 5 ]
+	then
+		profile_2p "$x1,$y1,$x2,$y2" "$srs" >> "$f5"
+		x1="$x2"
+		y1="$y2"
+		i=3
+		j=1
+	fi
+done
+
+# Last point
+lph="$(gdallocationinfo -valonly -l_srs "$srs" "$f_lidar" $x2 $y2)"
+
+# Result
+gawk -v lph="$lph" '
+BEGIN{
+	FS=","
+	l=0
+}
+{
+	if($2=="-9999.00")
+		$2=sprintf("%.0f",$2)
+	printf("%.2f %s\n",l,$2)
+	l=l+$1
+}
+END{
+	if(lph!="-9999")
+		lph=sprintf("%.2f",lph)
+	printf("%.2f %s\n",l,lph)
+}
+' "$f5"
+rm "$f5" 2> /dev/null
+
+exit 0
