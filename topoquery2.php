434a211168a426bc0b7712696bf0ed5168060d0e
juanmari <jcelihueta@gipuzkoa.eus>
2023-09-27 14:49:48 +0200

    Topoquery API: correction of a bug related to the featuretypes list

--- topoquery2.php
+++ topoquery2.php
@@ -432,7 +432,7 @@ if ($statuscode == 0 || $statuscode == 4 || $statuscode == 7) {
 }
 
 // Zoom restriction
-if (($z != "" || $featuretypenames != "") && ($statuscode != 3) && ($statuscode != 9)) {
+if (($z != "" || $featuretypenames != "") && ($statuscode != 3) && ($statuscode != 4) && ($statuscode != 9)) {
 	$data_json = file_get_contents($file_json);
 	$zoom_array = json_decode($data_json);
 	foreach ($zoom_array as $obj) {
