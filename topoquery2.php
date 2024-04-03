57b649c08dad80fe6bdfa76e4a12816025ab9405
juanmari <jcelihueta@gipuzkoa.eus>
2024-04-03 14:53:56 +0200

    Topoquery API: indent correction

--- topoquery2.php
+++ topoquery2.php
@@ -676,24 +676,24 @@ if ($statuscode == 0 || $statuscode == 7 || $statuscode == 9) {
 									if ($q2 != "idname" && $q2 != "type_eu" && $q2 != "type_es" && $q2 != "type_en" && $q2 != "class_eu" && $q2 != "class_es" && $q2 != "class_en" && $q2 != "id_poi" && $q2 != "class_description_eu" && $q2 != "class_description_es" && $q2 != "class_description_en" && $q2 != "category_eu" && $q2 != "category_es" && $q2 != "category_en" && $q2 != "category_description_eu" && $q2 != "category_description_es" && $q2 != "category_description_en" && $q2 != "poi_eu" && $q2 != "poi_es" && $q2 != "poi_en" && $q2 != "more_info_eu" && $q2 != "more_info_es" && $q2 != "more_info_en" && stripos($q2, "b5mcode_others") === false)
 										$doc2["features"][$q1]["properties"]["info"][0][$q2] = $wfs_response["features"][$q1]["properties"][$q2];
 
-										// Type
-										if ($q2 == "type_" . $lang)
-											$doc2["features"][$q1]["properties"]["info"][0]["type"] = $wfs_response["features"][$q1]["properties"][$q2];
+									// Type
+									if ($q2 == "type_" . $lang)
+										$doc2["features"][$q1]["properties"]["info"][0]["type"] = $wfs_response["features"][$q1]["properties"][$q2];
 
-										// Class and Category
-										if ($q2 == "class_" . $lang)
-											$doc2["features"][$q1]["properties"]["info"][0]["class"] = $wfs_response["features"][$q1]["properties"][$q2];
-										if ($q2 == "class_description_" . $lang)
-											$doc2["features"][$q1]["properties"]["info"][0]["class_description"] = $wfs_response["features"][$q1]["properties"][$q2];
-										if ($q2 == "category_" . $lang)
-											$doc2["features"][$q1]["properties"]["info"][0]["category"] = $wfs_response["features"][$q1]["properties"][$q2];
-										if ($q2 == "category_description_" . $lang)
-											$doc2["features"][$q1]["properties"]["info"][0]["category_description"] = $wfs_response["features"][$q1]["properties"][$q2];
-										if ($q2 == "poi_" . $lang)
-											$doc2["features"][$q1]["properties"]["info"][0]["poi"] = $wfs_response["features"][$q1]["properties"][$q2];
+									// Class and Category
+									if ($q2 == "class_" . $lang)
+										$doc2["features"][$q1]["properties"]["info"][0]["class"] = $wfs_response["features"][$q1]["properties"][$q2];
+									if ($q2 == "class_description_" . $lang)
+										$doc2["features"][$q1]["properties"]["info"][0]["class_description"] = $wfs_response["features"][$q1]["properties"][$q2];
+									if ($q2 == "category_" . $lang)
+										$doc2["features"][$q1]["properties"]["info"][0]["category"] = $wfs_response["features"][$q1]["properties"][$q2];
+									if ($q2 == "category_description_" . $lang)
+										$doc2["features"][$q1]["properties"]["info"][0]["category_description"] = $wfs_response["features"][$q1]["properties"][$q2];
+									if ($q2 == "poi_" . $lang)
+										$doc2["features"][$q1]["properties"]["info"][0]["poi"] = $wfs_response["features"][$q1]["properties"][$q2];
 
-										// More info
-										if ($q2 == "more_info_" . $lang)
+									// More info
+									if ($q2 == "more_info_" . $lang)
 											$doc2["features"][$q1]["properties"]["info"][0]["more_info"] = $wfs_response["features"][$q1]["properties"][$q2];
 								}
 							}
